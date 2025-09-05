89287041546b72360212c84468563a51dadd64a4
INEEDMONEY$ <121662005+INEEDDMONEYY@users.noreply.github.com>
2025-09-05 07:02:18 +0000

    Got backend server up & running although working with static files, will change to a DB when finalizing things. Will handle error functionality for server later in the future.

---
+++ throw-away/src/backend/data/posts.js
@@ -0,0 +1,52 @@
+const posts = [
+    {
+        id: 1,
+        author: "Sam",
+        title: "Ready?",
+        content: "NSFW",
+        date: "09/04/2025",
+        tags: [],
+    },
+    {
+        id: 2,
+        author: "Whisper",
+        title: "Snow bunny, petite, clean",
+        content: "NSFW",
+        date: "09/04/2025",
+        tags: [],
+    },
+    {
+        id: 3,
+        author: "Wonder",
+        title: "Come fuck this pussy bby",
+        content: "NSFW",
+        date: "09/04/2025",
+        tags: [],
+    },
+    {
+        id: 4,
+        author: "White Chocolate",
+        title: "Let me spoil you in all the ways i know how",
+        content: "NSFW",
+        date: "09/04/2025",
+        tags: [],
+    },
+    {
+        id: 5,
+        author: "Sydney The Freak",
+        title: "Are you horny, i'm ready to please!⛱️",
+        content: "NSFW",
+        date: "09/04/2025",
+        tags: [],
+    },
+    {
+        id: 6,
+        author: "Jasmine",
+        title: "Freaky latina, wet pussy, luxury vibes💝",
+        content: "NSFW",
+        date: "09/04/2025",
+        tags: [],
+    },
+]
+
+export default posts;
--- throw-away/src/backend/server.js
+++ throw-away/src/backend/server.js
@@ -1,13 +1,33 @@
-import express from 'express'
+//Imports
+import require from 'express'
+import post from './data/posts.js'
 //import dotnev from "dotenv"
-
-const app = express()
-const port = process.env.PORT || 4000
-
+import dotenv from 'dotenv'
+const app = require('express');
+const port = process.env.PORT || 4000;
+dotenv.config();
+//Server set up, implement error handlers
+///This message is logged to screen
 app.get('/', (req, res) => {
   res.send('This is the backend server that will handle data structure, error handling, and database mangement')
 })
-
+//This message is logged to the console
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+  console.log(`
+    https://Localhost:${port} Keep Grinding🔥!\n
+    Form posts should be served here Database intergration will be set up here\n
+    User authentication & User functionaility. Make sure backend folder is in the .gitignore file.\n 
+    Welcome to Mystery Mansion, where all naughty encounters are private 💝!
+    `)
+})
+
+//API
+//Creating an endpoint to get data from posts
+app.get('/api/posts', (req, res) => {
+  res.json(post)
+})
+//Getting posts by id*Getting posts by id not working needs a logic rework usings a different method function maybe filter()*
+app.get('/api/posts/:id', (req, res) => {
+  const pos = post.find((n) => n._id === req.params.id)
+  res.send(pos);
 })
